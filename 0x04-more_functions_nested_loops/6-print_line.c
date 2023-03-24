6bf3850fbcdb4eeaf10a98d610410c8d8f752b89
ZebNyar <zebnyazungu@yahoo.com>
2023-03-24 06:02:05 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Prints a straight line
+ * @n: number of lines
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+		_putchar('\n');
+}
