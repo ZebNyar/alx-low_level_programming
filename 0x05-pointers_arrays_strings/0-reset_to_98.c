30fcaedc9bb70d97aec77b654290930aace065b5
ZebNyar <zebnyazungu@yahoo.com>
2023-03-27 17:58:44 -0700

    resetto98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,14 @@
 #include "main.h"
-#include <stdio>
+#include <stdio.h>
+
 /**
- *8 reset_to_98 - function that takes a printer to a int
+ * reset_to_98 - function that takes a printer to a int
  * and updates its value to 98
  * @n: input
  * Return: n
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
