60f66185ca53a1fc211d69270dca7261084d31da
ZebNyar <zebnyazungu@yahoo.com>
2023-04-04 03:21:34 -0700

    reverse_alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - prints the lowercase alphabet in reverse form
+ * followed by a new line
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
